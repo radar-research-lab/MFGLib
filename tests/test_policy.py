206c611026feac8e68e27d346fee9a20d4e0da1c
Matteo Santamaria <matteosantama@gmail.com>
2023-04-13 11:55:57 -0700

    DO NOT MERGE, THIS IS A TEST

--- tests/test_policy.py
+++ tests/test_policy.py
@@ -5,26 +5,26 @@ from mfglib.env import Environment
 from mfglib.policy import Policy
 
 
-def test_policy() -> None:
-    lr = Environment.left_right()
-
-    pr = [
-        [0.5, 0.5],
-        [0.5, 0.5],
-        [0.5, 0.5],
-    ]
-
-    handcrafted = Policy([pr, pr])
-    stationary = Policy.stationary(pr)
-    automatic = Policy.uniform()
-
-    assert_close(handcrafted.build(lr), stationary.build(lr))
-    assert_close(stationary.build(lr), automatic.build(lr))
-
-    # test mismatched dimensions
-    with pytest.raises(ValueError):
-        Policy.stationary([[0.5, 0.5]]).build(lr)
-
-    # test invalid probability distribution
-    with pytest.raises(ValueError):
-        Policy.stationary([[1.0, 1.0], [1.0, 1.0], [1.0, 1.0]]).build(lr)
+# def test_policy() -> None:
+#     lr = Environment.left_right()
+#
+#     pr = [
+#         [0.5, 0.5],
+#         [0.5, 0.5],
+#         [0.5, 0.5],
+#     ]
+#
+#     handcrafted = Policy([pr, pr])
+#     stationary = Policy.stationary(pr)
+#     automatic = Policy.uniform()
+#
+#     assert_close(handcrafted.build(lr), stationary.build(lr))
+#     assert_close(stationary.build(lr), automatic.build(lr))
+#
+#     # test mismatched dimensions
+#     with pytest.raises(ValueError):
+#         Policy.stationary([[0.5, 0.5]]).build(lr)
+#
+#     # test invalid probability distribution
+#     with pytest.raises(ValueError):
+#         Policy.stationary([[1.0, 1.0], [1.0, 1.0], [1.0, 1.0]]).build(lr)
