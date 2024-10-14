add a temporary knob for float64/double precision on the package level + fix optuna fail_thresh corner case

mfglib/alg/mf_omo_obj.py
@@ -4,6 +4,8 @@ from typing import Literal
 
 import torch
 
+from mfglib import __TORCH_FLOAT__
+
 from mfglib.alg.mf_omo_params import mf_omo_params
 from mfglib.alg.utils import tuple_prod
 from mfglib.env import Environment
@@ -69,19 +71,21 @@ def mf_omo_obj(
     y = c_w * torch.sin(y_w) if parameterize else y_w
     b, A_L, c_L = mf_omo_params(env_instance, L)
 
+    # print(f"{y.dtype=}, {z.dtype=}, {L.dtype=}")
+
     # Compute the objective
-    obj = torch.zeros(1, dtype=torch.float)
+    obj = torch.zeros(1, dtype=torch.float64 if __TORCH_FLOAT__ == 64 else torch.float)
     if loss == "l1":
-        obj += c1 * (A_L.matmul(L.flatten().float()) - b).abs().sum()
-        obj += c2 * (A_L.transpose(0, 1).matmul(y.float()) + z - c_L).abs().sum()
+        obj += c1 * (A_L.matmul(L.flatten().double() if __TORCH_FLOAT__ == 64 else L.flatten().float()) - b).abs().sum()
+        obj += c2 * (A_L.transpose(0, 1).matmul(y.double() if __TORCH_FLOAT__ == 64 else y.float()) + z - c_L).abs().sum()
         obj += c3 * z.mul(L.flatten()).sum()
     if loss == "l2":
-        obj += c1 * (A_L.matmul(L.flatten().float()) - b).pow(2).sum()
-        obj += c2 * (A_L.transpose(0, 1).matmul(y.float()) + z - c_L).pow(2).sum()
+        obj += c1 * (A_L.matmul(L.flatten().double() if __TORCH_FLOAT__ == 64 else L.flatten().float()) - b).pow(2).sum()
+        obj += c2 * (A_L.transpose(0, 1).matmul(y.double() if __TORCH_FLOAT__ == 64 else y.float()) + z - c_L).pow(2).sum()
         obj += c3 * z.mul(L.flatten()).sum().pow(2)
     if loss == "l1_l2":
-        obj += c1 * (A_L.matmul(L.flatten().float()) - b).pow(2).sum()
-        obj += c2 * (A_L.transpose(0, 1).matmul(y.float()) + z - c_L).pow(2).sum()
+        obj += c1 * (A_L.matmul(L.flatten().double() if __TORCH_FLOAT__ == 64 else L.flatten().float()) - b).pow(2).sum()
+        obj += c2 * (A_L.transpose(0, 1).matmul(y.double() if __TORCH_FLOAT__ == 64 else y.float()) + z - c_L).pow(2).sum()
         obj += c3 * z.mul(L.flatten()).sum()
 
     return obj

mfglib/alg/q_fn.py
@@ -4,6 +4,8 @@ from typing import Callable, cast
 
 import torch
 
+from mfglib import __TORCH_FLOAT__
+
 from mfglib.env import Environment
 
 
@@ -115,6 +117,6 @@ class QFn:
                 .sum(dim=-1)
                 .flatten()
             )
-            return self._transition_probabilities(t) @ pi_q.float()
+            return self._transition_probabilities(t) @ (pi_q.double() if __TORCH_FLOAT__ == 64 else pi_q.float())
 
         return self._compute_q_values(future_rewards)

mfglib/env/examples/random_linear.py
@@ -4,23 +4,25 @@ from typing import cast
 
 import torch
 
+from mfglib import __TORCH_FLOAT__
+
 from mfglib.env import Environment
 
 
 class TransitionFn:
     def __init__(self, n: int, m: float) -> None:
-        self.p1 = 2 * m * torch.rand(n, n, n, dtype=torch.float) - m
-        self.p2 = 2 * m * torch.rand(n, n, n, dtype=torch.float) - m
+        self.p1 = 2 * m * torch.rand(n, n, n, dtype=torch.float64 if __TORCH_FLOAT__ == 64 else torch.float) - m
+        self.p2 = 2 * m * torch.rand(n, n, n, dtype=torch.float64 if __TORCH_FLOAT__ == 64 else torch.float) - m
 
     def __call__(self, env: Environment, t: int, L_t: torch.Tensor) -> torch.Tensor:
         soft_max = torch.nn.Softmax(dim=0)
-        return cast(torch.Tensor, soft_max(self.p1 @ L_t.float() + self.p2))
+        return cast(torch.Tensor, soft_max(self.p1 @ (L_t.double() if __TORCH_FLOAT__ == 64 else L_t.float()) + self.p2))
 
 
 class RewardFn:
     def __init__(self, n: int, m: float) -> None:
-        self.r1 = 2 * m * torch.rand(n, n, dtype=torch.float) - m
-        self.r2 = 2 * m * torch.rand(n, n, dtype=torch.float) - m
+        self.r1 = 2 * m * torch.rand(n, n, dtype=torch.float64 if __TORCH_FLOAT__ == 64 else torch.float) - m
+        self.r2 = 2 * m * torch.rand(n, n, dtype=torch.float64 if __TORCH_FLOAT__ == 64 else torch.float) - m
 
     def __call__(self, env: Environment, t: int, L_t: torch.Tensor) -> torch.Tensor:
-        return self.r1 @ L_t.float() + self.r2
+        return self.r1 @ (L_t.double() if __TORCH_FLOAT__ == 64 else L_t.float()) + self.r2