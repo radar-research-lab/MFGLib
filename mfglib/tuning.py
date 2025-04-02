f05b33c3c4db4b16223f9e5c5201229a2ea1756c
Matteo Santamaria <matteosantama@gmail.com>
2025-04-02 09:45:37 -0700

    Overhaul Algorithms + Environments pages

--- mfglib/tuning.py
+++ mfglib/tuning.py
@@ -27,59 +27,54 @@ class Metric(Protocol):
 
         Args
         ----
-            pis
-                A list-of-list of policies. Each item in the outer list
-                represents an (environment, policy) pair, and each item
-                in the inner list corresponds with an algorithm iteration.
-            expls
-                A list-of-list of exploitability scores. Each item in the
-                outer list represents an (environment, policy) pair, and
-                each item in the inner list corresponds with an algorithm
-                iteration.
-            rts
-                A list-of-list of runtimes. Each item in the outer list represents
-                an (environment, policy) pair, and each item in the inner list
-                corresponds with an algorithm iteration.
-            solve_kwargs:
-                Additional arguments passed to ``solve()``.
-
-        Returns
-        -------
-        float
-            A scalar measure for the provided polices, exploitability scores, and
-            runtimes.
+        pis
+            A list-of-list of policies. Each item in the outer list
+            represents an (environment, policy) pair, and each item
+            in the inner list corresponds with an algorithm iteration.
+        expls
+            A list-of-list of exploitability scores. Each item in the
+            outer list represents an (environment, policy) pair, and
+            each item in the inner list corresponds with an algorithm
+            iteration.
+        rts
+            A list-of-list of runtimes. Each item in the outer list represents
+            an (environment, policy) pair, and each item in the inner list
+            corresponds with an algorithm iteration.
+        solve_kwargs:
+            Additional arguments passed to ``solve()``.
 
         """
         ...
 
 
 class FailureRate:
+    """Failure rate metric.
+
+    Parameters
+    ----------
+    fail_thresh
+        The failure threshold used to identify if the solver 'failed'
+        on a particular environment instance. This threshold is compared
+        against the chosen statistic (``stat``). For instance, if
+        ``stat="iter"``, it defines the maximum number of iterations
+        the solver is allowed to run before considering it to have failed. If
+        ``None``, the default threshold is determined based on the value of
+        ``stat``.
+    stat
+        The statistic to monitor during optimization. It can be one
+        of the following:
+
+        - ``iter``: the number of iterations taken by the solver.
+        - ``rt``: the runtime (in seconds) taken by the solver.
+        - ``expl``: the minimum exploitability score observed during
+          the solution process.
+    """
+
     def __init__(
         self,
         fail_thresh: float | int | None = None,
         stat: Literal["iter", "rt", "expl"] = "expl",
     ) -> None:
-        """Failure rate metric.
-
-        Args
-        ----
-            fail_thresh
-                The failure threshold used to identify if the solver 'failed'
-                on a particular environment instance. This threshold is compared
-                against the chosen statistic (``stat``). For instance, if
-                ``stat="iter"``, it defines the maximum number of iterations
-                the solver is allowed to run before considering it to have failed. If
-                ``None``, the default threshold is determined based on the value of
-                ``stat``.
-            stat
-                The statistic to monitor during optimization. It can be one
-                of the following:
-
-                - ``iter``: the number of iterations taken by the solver.
-                - ``rt``: the runtime (in seconds) taken by the solver.
-                - ``expl``: the minimum exploitability score observed during
-                  the solution process.
-        """
         if stat not in ("iter", "rt", "expl"):
             raise ValueError("stat must be 'iter' or 'rt' or 'expl'")
         self.stat = stat
@@ -120,24 +115,25 @@ class FailureRate:
 
 
 class GeometricMean:
+    """Shifted geometric mean metric.
+
+    Parameters
+    ----------
+    shift
+        An additional shift value for the geometric mean. Defaults to zero.
+    stat
+        The statistic to monitor during optimization. It can be one
+        of the following:
+
+        - ``iter``: the number of iterations taken by the solver.
+        - ``rt``: the runtime (in seconds) taken by the solver.
+        - ``expl``: the minimum exploitability score observed during
+          the solution process.
+    """
+
     def __init__(
         self, shift: float = 0, stat: Literal["iter", "rt", "expl"] = "expl"
     ) -> None:
-        """Geometric mean metric.
-
-        Args
-        ----
-            shift
-                An additional shift value for the geometric mean. Defaults to zero.
-            stat
-                The statistic to monitor during optimization. It can be one
-                of the following:
-
-                - ``iter``: the number of iterations taken by the solver.
-                - ``rt``: the runtime (in seconds) taken by the solver.
-                - ``expl``: the minimum exploitability score observed during
-                  the solution process.
-        """
         if stat not in ("iter", "rt", "expl"):
             raise ValueError("stat must be 'iter' or 'rt' or 'expl'")
         self.stat = stat
